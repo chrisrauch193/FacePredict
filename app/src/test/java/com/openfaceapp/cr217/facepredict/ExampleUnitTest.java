bbcdbfa80b305d3a796b1e7737850ff29a8cc977
chrisrauch193 <cr217@st-andrews.ac.uk>
2017-03-29 14:54:47 +0100

    Adding simple interface with work camera functionality

---
+++ app/src/test/java/com/openfaceapp/cr217/facepredict/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.openfaceapp.cr217.facepredict;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
